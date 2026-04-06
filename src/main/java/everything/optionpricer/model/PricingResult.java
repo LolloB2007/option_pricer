880f4a8b44f1f40e620c0574c9db5647aad0167f
Lorenzo <lollo.barbagelata@gmail.com>
2026-04-06 22:40:21 +0200

    technically finished BlackScholesEngine

--- src/main/java/everything/optionpricer/model/PricingResult.java
+++ src/main/java/everything/optionpricer/model/PricingResult.java
@@ -15,6 +15,14 @@ public class PricingResult {
     private double volatility;
     private double currentPrice;
     
+    
+    /**
+     * Not sure how this constructor is going to be used
+     * @param option
+     * @param r
+     * @param v
+     * @param p 
+     */
     public PricingResult(Option option, double r, double v, double p) {
         this.option = option;
         this.riskFreeRate = r;
@@ -22,12 +30,20 @@ public class PricingResult {
         this.currentPrice = p;
     }
     
+    
+    /**
+     * Method that prices the option depending on CALL or PUT
+     * @return double
+     */
     public double price() {
         
         OptionType type = option.getOptionType();
         
         if(type == CALL) {
             double d1 = BlackScholesEngine.d1(currentPrice, option.getStrikePrice(), riskFreeRate, volatility, option.getTimeToExpiry());
+            double d2 = (d1 - (volatility*Math.sqrt(option.getTimeToExpiry())));
+            
+            return BlackScholesEngine.cost(currentPrice, option.getStrikePrice(), riskFreeRate, option.getTimeToExpiry(), d1, d2, option.getOptionType());
         }
         
         return 0.0; //just to prevent IDE error from being annoying
--- src/main/java/everything/optionpricer/pricing/BlackScholesEngine.java
+++ src/main/java/everything/optionpricer/pricing/BlackScholesEngine.java
@@ -1,5 +1,9 @@
 package everything.optionpricer.pricing;
 
+import everything.optionpricer.model.OptionType;
+import static everything.optionpricer.model.OptionType.CALL;
+import everything.optionpricer.util.NormalDistribution;
+
 
 /**
  * Engine for computation using BlackScholes
@@ -7,11 +11,44 @@ package everything.optionpricer.pricing;
  */
 public class BlackScholesEngine {
     
+    
+    /**
+     * Calculates d1 parameter for pricing
+     * @param S
+     * @param K
+     * @param r
+     * @param v
+     * @param t
+     * @return double
+     */
     public static double d1(double S, double K, double r, double v, double t)
     {
         double num = (Math.log(S/K) + ((r + (1/2)*Math.pow(v, 2))*(t)));
         double denom = (v * Math.sqrt(t));
         
         return (num/denom);
+    }
+    
+    
+    /**
+     * Method that returns current price of option
+     * @param S
+     * @param K
+     * @param r
+     * @param t
+     * @param d1
+     * @param d2
+     * @return 
+     */
+    public static double cost(double S, double K, double r, double t, double d1, double d2, OptionType type) {
+        
+        if(type == CALL) {
+            double partOne = S * NormalDistribution.cdf(d1);
+            double partTwo = K * Math.pow(Math.E, (-r * t)) * NormalDistribution.cdf(d2);
+
+            return partOne - partTwo;
+        }
+        
+        return 0.0; //temporarily added
     }
 }
