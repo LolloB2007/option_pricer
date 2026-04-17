d12f8dabce1a27a8412bfc503eaee2b87dbc6836
Lorenzo <lollo.barbagelata@gmail.com>
2026-04-17 17:53:10 +0200

    added components to MainFrame

--- src/main/java/everything/optionpricer/Main.java
+++ src/main/java/everything/optionpricer/Main.java
@@ -1,10 +1,8 @@
 package everything.optionpricer;
 
-
-import everything.optionpricer.model.*;
-import everything.optionpricer.pricing.*;
-import java.util.InputMismatchException;
-import java.util.Scanner;
+import com.formdev.flatlaf.FlatDarkLaf;
+import everything.optionpricer.gui.MainFrame;
+import javax.swing.SwingUtilities;
 
 /**
  * entry point
@@ -14,53 +12,14 @@ public class Main {
 
     public static void main(String[] args) {
         
-        Scanner input = new Scanner(System.in);
+        FlatDarkLaf.setup();
         
-        System.out.println("=== Black-Scholes European Option Pricer ===");
-        System.out.println("Enter rates and volatility as decimals:");
-        System.out.println("ex. 6% --> 0.06; 15% --> 0.15");
-        System.out.println("");
+        SwingUtilities.invokeLater( () -> {
+            MainFrame main = new MainFrame();
+            main.setSize(400, 500);
+            main.setLocationRelativeTo(null);
+            main.setVisible(true);
+        });
         
-        try{
-            System.out.print("Option type (CALL/PUT): ");
-            String type = input.nextLine().trim().toUpperCase();
-
-            System.out.print("Spot price (S): ");
-            double s = input.nextDouble();
-
-            System.out.print("Strike price (K): ");
-            double k = input.nextDouble();
-
-            System.out.print("Risk-free rate (r): ");
-            double r = input.nextDouble();
-
-            System.out.print("Volatility (sigma): ");
-            double v = input.nextDouble();
-
-            System.out.print("Time to expire in years (T): ");
-            double t = input.nextDouble();
-
-            Option option = new Option();
-
-            if(type.equals("CALL")) {
-                option = Option.call(k, t);
-            } else if(type.equals("PUT")) {
-                option = Option.put(k, t);
-            } else {
-                throw new IllegalArgumentException("Please enter a valid European Option type");
-            }
-            
-            if(t<=0) {
-                throw new IllegalArgumentException("Please enter a valid time to expiry");
-            }
-
-            PricingResult price = BlackScholesEngine.price(option, s, r, v);
-
-            System.out.println();
-            System.out.println("=== Result ===");
-            System.out.print(price);
-        } catch(InputMismatchException e) {
-            System.out.println("Please enter a valid value");
-        }
     }
 }
---
+++ src/main/java/everything/optionpricer/gui/MainFrame.java
@@ -0,0 +1,148 @@
+package everything.optionpricer.gui;
+
+import javax.swing.*;
+import net.miginfocom.swing.MigLayout;
+
+
+/**
+ * special case of JFrame. Entry point of gui
+ * @author lorenzobarbagelata
+ */
+public class MainFrame extends JFrame {
+    
+    private JPanel MainPanel;
+    
+    private JComboBox optionTypeCombo;
+    private JTextField spotField;
+    private JTextField strikeField;
+    private JTextField rateField;
+    private JTextField volField;
+    private JTextField timeField;
+    
+    private JButton priceButton;
+    private JLabel resultLabel;
+    
+    
+    /**
+     * Constructor for MainFrame
+     */
+    public MainFrame() {
+        MainPanel = new JPanel(new MigLayout(
+                "fillx, insets 20",
+                "[right]12[grow, fill]",
+                "[]10[]10[]10[]10[]10[]20[]"
+        ));
+        
+        componentInit();
+        
+        MainPanel.add(new JLabel("Option Type:"));
+        MainPanel.add(optionTypeCombo, "wrap");
+
+        MainPanel.add(new JLabel("Spot Price (S):"));
+        MainPanel.add(spotField, "wrap");
+
+        MainPanel.add(new JLabel("Strike Price (K):"));
+        MainPanel.add(strikeField, "wrap");
+
+        MainPanel.add(new JLabel("Risk-Free Rate (r), decimal form:"));
+        MainPanel.add(rateField, "wrap");
+
+        MainPanel.add(new JLabel("Volatility (σ), decimal form:"));
+        MainPanel.add(volField, "wrap");
+
+        MainPanel.add(new JLabel("Time to Expiry (T):"));
+        MainPanel.add(timeField, "wrap");
+
+        MainPanel.add(priceButton, "span 2, center, wrap");
+        MainPanel.add(resultLabel, "span 2, growx");
+    }
+    
+    
+    /**
+     * Method that initialises all components of MainFrame
+     */
+    private void componentInit() {
+        optionTypeInit();
+        spotFieldInit();
+        strikeFieldInit();
+        rateFieldInit();
+        volFieldInit();
+        timeFieldInit();
+        priceButtonInit();
+        resultLabelInit();
+    }
+    
+    
+    /**
+     * Initialises optionType JComboBox
+     */
+    private void optionTypeInit() {
+        String[] options = {"Call", "Put"};
+        optionTypeCombo = new JComboBox(options);
+        optionTypeCombo.setVisible(true);
+    }
+    
+    
+    /**
+     * Initialises spotField JTextField
+     */
+    private void spotFieldInit() {
+        spotField = new JTextField(8);
+        spotField.setVisible(true);
+    }
+    
+    
+    /**
+     * Initialises strikeField JTextField
+     */
+    private void strikeFieldInit() {
+        strikeField = new JTextField(8);
+        strikeField.setVisible(true);
+    }
+    
+    
+    /**
+     * Initialises rateField JTextField
+     */
+    private void rateFieldInit() {
+        rateField = new JTextField(8);
+        rateField.setVisible(true);
+    }
+    
+    
+    /**
+     * Initialises volField JTextField
+     */
+    private void volFieldInit() {
+        volField = new JTextField(8);
+        volField.setVisible(true);
+    }
+    
+    
+    /**
+     * Initialises timeField JTextField
+     */
+    private void timeFieldInit() {
+        timeField = new JTextField(8);
+        timeField.setVisible(true);
+    }
+    
+    
+    /**
+     * Initialises priceButton JButton
+     */
+    private void priceButtonInit() {
+        priceButton = new JButton("Price option");
+        priceButton.setVisible(true);
+    }
+    
+    
+    /**
+     * Initialises resultLabel JLabel and sets its visibility to false
+     */
+    private void resultLabelInit() {
+        resultLabel = new JLabel("");
+        resultLabel.setVisible(false);
+    }
+    
+}
