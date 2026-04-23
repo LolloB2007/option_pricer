79bc1348e9dad761a506e99f9e9d7f2ff1416272
Lorenzo <lollo.barbagelata@gmail.com>
2026-04-23 23:31:31 +0200

    started work on MonteCarlo

--- src/main/java/everything/optionpricer/pricing/MonteCarloEngine.java
+++ src/main/java/everything/optionpricer/pricing/MonteCarloEngine.java
@@ -1,9 +1,40 @@
 package everything.optionpricer.pricing;
 
+import everything.optionpricer.model.*;
+import everything.optionpricer.util.*;
+
 /**
- * Engine for computation using MonteCarlo simulations
+ * Engine for computation using Monte Carlo simulations
  * @author lorenzobarbagelata
  */
 public class MonteCarloEngine {
+
+    private static final int DEFAULT_SIMULATIONS = 100000;
     
+    
+    /**
+     * Caller method for pricing
+     * @param option
+     * @param currentPrice
+     * @param riskFreeRate
+     * @param volatility
+     * @return PricingResult
+     */
+    public static PricingResult price(PathDependentOption option, double currentPrice, double riskFreeRate, double volatility) {
+        return price(option, currentPrice, riskFreeRate, volatility, DEFAULT_SIMULATIONS);
+    }
+    
+    
+    /**
+     * Actual pricing computation
+     * @param o
+     * @param S
+     * @param r
+     * @param v
+     * @param sims
+     * @return PricingResult
+     */
+    private static PricingResult price(PathDependentOption o, double S, double r, double v, int sims) {
+        //some code. will do shortly, prolly tomorrow
+    }
 }
