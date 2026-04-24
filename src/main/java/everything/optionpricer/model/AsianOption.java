41a208a49d00b5df549ab638c0182e9e3e9f7946
Lorenzo <lollo.barbagelata@gmail.com>
2026-04-24 22:53:46 +0200

    finished Asian options

--- src/main/java/everything/optionpricer/model/AsianOption.java
+++ src/main/java/everything/optionpricer/model/AsianOption.java
@@ -42,7 +42,56 @@ public class AsianOption extends PathDependentOption {
      * @return double
      */
     @Override
-    public double payoff(LinkedList<Double> path) {
-        return 0.0;
+    public double payoff(LinkedList path) {
+        double average;
+        
+        if(this.arithmeticAverage) {
+            average = computeArithmeticAverage(path);
+        } else {
+            average = computeGeometricAverage(path);
+        }
+        
+        if(this.isCall()) {
+            return Math.max(average - this.getStrikePrice(), 0.0);
+        } else {
+            return Math.max(this.getStrikePrice() - average, 0.0);
+        }
+    }
+
+    
+    /**
+     * Computes arithmetic average of prices in path
+     * Helper to payoff
+     * @param path
+     * @return double
+     */
+    private double computeArithmeticAverage(LinkedList path) {
+        int length = path.size();
+        double total = 0.0;
+        
+        for(int i = 0; i<length; i++) {
+            total += (double) path.get(i);
+        }
+        
+        return (total/length);
+    }
+
+    
+    /**
+     * Computes geometric average of prices in path
+     * Uses log addition to avoid issues with numbers getting too big for java
+     * Helper to payoff
+     * @param path
+     * @return double
+     */
+    private double computeGeometricAverage(LinkedList path) {
+        int length = path.size();
+        double lnSum = 0.0;
+        
+        for(int i = 0; i<length; i++) {
+            lnSum = Math.log((double) path.get(i));
+        }
+        
+        return (Math.exp(lnSum/length));
     }
 }
